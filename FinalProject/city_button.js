86ae56c2264ef8d1e578160dd1d14309a296ce6d
ClaireUmp <cumpleby20@nscds.org>
2019-12-10 04:34:48 +0000

    more css

--- FinalProject/city_button.js
+++ FinalProject/city_button.js
@@ -1,3 +1,7 @@
 $("#citybutton").click(function(){
      $(".Menu").css("background-color", "red");
   });
+
+  for(var d=1; d<=5; d++){
+    $("#buildingWrapper").append("<div class='building' id= 'build" + d + "'></div>");
+}
