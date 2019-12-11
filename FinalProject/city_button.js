097803715578b950b5196931371609f72501a337
ClaireUmp <cumpleby20@nscds.org>
2019-12-11 17:30:03 +0000

    city css

--- FinalProject/city_button.js
+++ FinalProject/city_button.js
@@ -1,7 +1,7 @@
-$("#citybutton").click(function(){
+/*$("#citybutton").click(function(){
      $(".Menu").css("background-color", "red");
-  });
+  }); */
 
   for(var d=1; d<=5; d++){
-    $("#buildingWrapper").append("<div class='building' id= 'build" + d + "'></div>");
+    $("#buildingWrapper").append("<div class='building' id= 'build'" + d + "'></div>");
 }
