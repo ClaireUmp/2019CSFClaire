9405e3404e32b5454c440f8d15abfc111f1706a3
ClaireUmp <cumpleby20@nscds.org>
2019-12-13 15:27:11 +0000

    no longer infinite loop

--- FinalProject/city_button.js
+++ FinalProject/city_button.js
@@ -3,5 +3,5 @@
   }); */
 
   for(var d=1; d<=5; d++){
-    $("#buildingWrapper").append("<div class='building' id= 'build'" + d + "'></div>");
-}
+    $('.buildingWrapper').append("<div class='building' id= 'build" + d + "></div>");
+};
--- FinalProject/flower_button.js
+++ FinalProject/flower_button.js
@@ -1,98 +1,12 @@
 $("#flowerbutton").click(function(){
-    for(var b=1; b<=18; c++){
-    $("#flowerWrapper").append("<div class='petals' id= 'petal" + b + "'></div>");
-}
-    /* $("#flowers_main").append("<div class='petals' id= 'petal17'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal15'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal13'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal11'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal9'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal7'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal5'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal3'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal1'></div>");
-     
-     $("#flowers_main").append("<div class='petals' id= 'petal4'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal6'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal8'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal10'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal12'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal14'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal16'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal2'></div>");
-     $("#flowers_main").append("<div class='petals' id= 'petal18'></div>");
-     */
-     
-     $("#flowers_main").append("<div class='petals2' id= 'petal17'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal15'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal13'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal11'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal9'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal7'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal5'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal3'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal1'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal4'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal6'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal8'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal10'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal12'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal14'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal16'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal2'></div>");
-     $("#flowers_main").append("<div class='petals2' id= 'petal18'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal17'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal15'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal13'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal11'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal9'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal7'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal5'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal3'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal1'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal4'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal6'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal8'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal10'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal12'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal14'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal16'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal2'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'petal18'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal17'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal15'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal13'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal11'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal9'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal7'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal5'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal3'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal1'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal4'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal6'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal8'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal10'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal12'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal14'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal16'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal2'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'petal18'></div>");
-     $("#flowers_main").append("<div class='petals4' id= 'center4'></div>");
-     $("#flowers_main").append("<div class='petals3' id= 'center3'</div>");
-     $("#flowers_main").append("<div class='petals2' id= 'center2'</div>");
-     $("#flowers_main").append("<div class='petals' id= 'center'</div>");
- 
- $('.petals').click(function(){
-    console.log(color);
+    for(var p=1; p<=4; p++)
+        for(var b=1; b<=18; b++){
+            $("#flowerWrapper").append("<div class='flower" + p + "id= 'petal" + b + "'></div>");
+        }
+});
+  
+ $("#flowerWrapper").click(function(){
     $(this).css("background-color", color);
     });
- $('.petals2').click(function(){
-    $(this).css("background-color", color);
-});
-$('.petals3').click(function(){
-    $(this).css("background-color", color);
-});
-$('.petals4').click(function(){
-    $(this).css("background-color", color);
-});
- });
+
+ 
