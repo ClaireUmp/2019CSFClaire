01968451543ae8b6688cd7b0b08d42b9bcc3964a
ClaireUmp <cumpleby20@nscds.org>
2019-12-18 19:20:57 +0000

    windows

--- FinalProject/city_button.js
+++ FinalProject/city_button.js
@@ -3,5 +3,16 @@
   }); */
 
   for(var d=1; d<=5; d++){
+      console.log(d);
     $('.buildingWrapper').append("<div class='building' id= 'build" + d + "'></div>");
+};
+
+for(var e=1; e<=7; e++){
+      console.log(e);
+    $('.doorWrapper').append("<div class='doors' id= 'door" + e + "'></div>");
+};
+
+
+for(var f=1; f<=21; f++){
+        $('#build2').append("<div class='all_windows'></div>");
 };
